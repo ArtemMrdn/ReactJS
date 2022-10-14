d2b8d416e58f8ef42adeab1de0666400a5f5bcb1
ArtomMrdn <artommrdn47@gmail.com>
2022-10-14 12:20:18 +0300

    lesson204 added

--- 190-203-todo-app-v2/src/components/Todos/TodosActions.js
+++ 190-203-todo-app-v2/src/components/Todos/TodosActions.js
@@ -1,7 +1,11 @@
 import { RiDeleteBin2Line, RiRefreshLine } from "react-icons/ri";
 import Button from "../UI/Button";
 
-function TodosActions({ resetTodos, deleteCompletedTodos }) {
+function TodosActions({
+  resetTodos,
+  deleteCompletedTodos,
+  completedTodosExist,
+}) {
   return (
     <>
       <Button title="Reset Todos" onClick={resetTodos}>
@@ -10,6 +14,7 @@ function TodosActions({ resetTodos, deleteCompletedTodos }) {
       <Button
         title="Clear Completed Todos"
         onClick={deleteCompletedTodos}
+        disabled={!completedTodosExist}
       >
         <RiDeleteBin2Line />
       </Button>
